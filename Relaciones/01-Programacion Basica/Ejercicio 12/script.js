ccca265e3d7104d011791bce10b6fb905e84e9c7
ipricez <fsfcantarero@gmail.com>
2024-09-25 19:56:07 +0200

    Ejercicio 12 hecho en casa

--- Relaciones/01-Programacion Basica/Ejercicio 12/script.js
+++ Relaciones/01-Programacion Basica/Ejercicio 12/script.js
@@ -1,1 +1,20 @@
 // Definición de la función
+function filtroColores(arrayA,arrayB) {
+    
+    let coloresEncontrados = [];
+    let noColores = [];
+
+    // Recorrer el array 'usuario' y separar colores y no colores
+    for (let i = 0; i < usuario.length; i++) {
+        if (colores.includes(usuario[i])) {
+            coloresEncontrados.push(usuario[i]); // Si es un color, lo añadimos a coloresEncontrados
+        } else {
+            noColores.push(usuario[i]); // Si no es un color, lo añadimos a noColores
+        }
+    }
+
+    // Concatenar colores y no colores
+    return coloresEncontrados.concat(noColores);
+
+
+}
