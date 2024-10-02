b9ba1e7db3e28945bdb1972655ef247fe58ba222
ipricez <fsfcantarero@gmail.com>
2024-10-02 22:34:17 +0200

    ejercicio 11

--- README.md
+++ README.md
@@ -2,5 +2,7 @@ Este es el repositorio de la asignatura Desarrollo Web Cliente
 
 Jaime actualizó la relación y tengo que ver si el numero de ejercicios coincide.
 Ejercicios repasados =>
-=> 1,2,3,4,,6,7,8
+=> 1,2,3,4,,6,7,8,9,10,11
+
+EL EJERCICIO 9 LO TENGO QUE MIRAR
 Ejercicio 6 tengo dudas sobre si está bien
---
+++ Relaciones/01-Programacion Basica/Ejercicio 11/script.js
@@ -0,0 +1,24 @@
+/*
+    Crear una función que traduzca una cadena de ADN y ARN.
+    Por ejemplo:
+        Si se introduce la cadena GCAT, a salida debe ser GCAU
+        Si se introduce la cadena GCATCGTA, a salida debe ser GCAUCGUA
+*/
+function quimica(cadenaADN) {
+    
+    // Creo la cadena ARN
+    let cadenaARN ="";
+
+    return cadenaADN.replaceAll('T','U');
+
+    // Recorro la cadena
+    // for (let i = 0; i < cadenaADN.length; i++) {
+    //     const element = cadenaADN[i];
+    //     if (element=='T') cadenaARN +='U';
+    //     else cadenaARN += element;
+    // }
+    //return cadenaARN;    
+}
+
+
+console.log(quimica("GCAT"));
